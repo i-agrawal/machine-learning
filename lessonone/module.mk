16969363cef12e7d4e54859129163987e8ad3160
Ishan Agrawal <ishan.agrawal97@gmail.com>
2017-08-04 23:43:48 -0700

    lowercased

--- lessonone/module.mk
+++ lessonone/module.mk
@@ -1,3 +1,3 @@
 lessonone_prog := lone
 lessonone_modules := grapher matrices
-lessonone_cf := -D DATAFILE=\"$(realpath lessonone/res/linear.dat)\"
+lessonone_cf := -D datafile=\"$(realpath lessonone/res/linear.dat)\"
